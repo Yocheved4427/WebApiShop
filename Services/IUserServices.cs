3091802bf64d9a8d81411e95abaec671544a9ed5
seeandbesharp-code-review <38328279757@mby.co.il>
2025-12-10 13:54:41 +0200

    Code Review - 10-12-25

--- Services/IUserServices.cs
+++ Services/IUserServices.cs
@@ -1,13 +1,13 @@
 ﻿using Entities;
 
-namespace Services
+namespace UserServices
 {
     public interface IUserServices
     {
         Task<User?> GetUserById(int id);
         Task<User?> Login(ExistingUser existingUser);
         Task<User?> Register(User user);
-        Task<bool> Upadate(int id, User updateUser);
+        Task<bool> Update(int id, User updateUser);
         Task<IEnumerable<User>> GetUsers();
     }
 }
--- Services/UserServices.cs
+++ Services/UserServices.cs
@@ -31,22 +31,13 @@ namespace Services
                 return null;
             return await _repository.Register(user);
         }
-        public async Task<bool> Upadate(int id, User updateUser)
+        public async Task<bool> Update(int id, User updateUser)
         {
             int passScore = _passwordServices.PasswordScore(updateUser.Password);
             if (passScore < 2)
                 return false;
-            await _repository.Upadate(id, updateUser);
+            await _repository.Update(id, updateUser);
             return true;
-
-        }
-        
-
-       
-
-        public int PasswordScore(string password)
-        {
-            throw new NotImplementedException();
         }
 
         
